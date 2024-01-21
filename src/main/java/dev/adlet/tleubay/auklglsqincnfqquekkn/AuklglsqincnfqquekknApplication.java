60f05a87a742c61bf62013b94c0fdf1926905bed
Adlet777 <iadlet79@gmail.com>
2024-01-22 03:48:24 +0600

    initial commit

---
+++ src/main/java/dev/adlet/tleubay/auklglsqincnfqquekkn/AuklglsqincnfqquekknApplication.java
@@ -0,0 +1,13 @@
+package dev.adlet.tleubay.auklglsqincnfqquekkn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuklglsqincnfqquekknApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuklglsqincnfqquekknApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/adlet/tleubay/auklglsqincnfqquekkn/AuklglsqincnfqquekknApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adlet.tleubay.auklglsqincnfqquekkn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuklglsqincnfqquekknApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
