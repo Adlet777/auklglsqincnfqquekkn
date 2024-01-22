f91309e99ad9f1afa5335d3848fe58f0ae5b7f7e
Adlet777 <iadlet79@gmail.com>
2024-01-22 23:21:45 +0600

    major fix: removed docker-compose realisation due to RAM issues. added online PostgreSQL instance instead

--- src/main/java/dev/adlet/tleubay/auklglsqincnfqquekkn/entity/PostgresTelephony.java
+++ src/main/java/dev/adlet/tleubay/auklglsqincnfqquekkn/entity/PostgresTelephony.java
@@ -4,9 +4,5 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "telephony")
-public class PostgresTelephony {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+public class PostgresTelephony extends Telephony {
 }
